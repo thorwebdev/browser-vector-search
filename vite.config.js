c55d9fd34f614ac16e630e2cc14e1dfa0531d2ef
thorwebdev <thor@supabase.io>
2024-08-15 17:25:02 +0800

    fix: vite base config for gh pages.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/browser-vector-search/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['@electric-sql/pglite'],
